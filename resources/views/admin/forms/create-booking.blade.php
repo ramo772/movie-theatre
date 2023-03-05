dbe488f57655c74ab0a7ebd07464bf32e1e1ecd6
LAPTOP-QV4BMLN3\Asus <omarkhaledibraheem@gmail.com>
2023-03-05 21:02:29 +0200

    form

--- resources/views/admin/forms/create-booking.blade.php
+++ resources/views/admin/forms/create-booking.blade.php
@@ -2,16 +2,16 @@
 <x-input label="Email" name="email" type="email" required />
 <x-input label="Mobile" name="mobile" type="number" required />
 
-<label class="form-label form-label-lg " for="FirstName">Day</label>
-<select id="sel_day-{{ $movie->id }}" name="day_id" class="form-control" data-movie-id="{{ $movie->id }}">
+<label class="form-label form-label-lg " for="FirstName">Day <span style="color:red">*</span></label>
+<select id="sel_day-{{ $movie->id }}" required name="day_id" class="form-control" data-movie-id="{{ $movie->id }}">
     <option value="">Select Day</option>
     @foreach ($movie->event->unique('day') as $event)
         <option value="{{ $event->day->id }}">{{ $event->day->day }}</option>
         {{-- <option   value="{{$show_time->id}}">{{ $show_time->start_at }} - {{ $show_time->end_at }}</option> --}}
     @endforeach
 </select>
-<label class="form-label form-label-lg " for="FirstName">Show Time</label>
+<label class="form-label form-label-lg " for="FirstName">Show Time<span style="color:red">*</span></label>
 <input value="{{ $movie->id }}" name="movie_id" hidden />
-<select name="show_time_id" class="form-control" id="sel_time-{{ $movie->id }}">
+<select name="show_time_id" required class="form-control" id="sel_time-{{ $movie->id }}">
     <option value="">Select Show Time</option>
 </select>
